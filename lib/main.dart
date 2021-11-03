e7289fef7aea0592af613abc47ccb2f3415e58ef
mburgos012576 <embq28@gmail.com>
2021-11-02 20:56:23 -0500

    provider service

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo3_state/pages/info_superhero_page.dart';
+import 'package:flutter_codigo3_state/services/superheroe_services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo - Singleton',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (BuildContext context)=> SuperheroService()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: InfoSuperheroPage(),
       ),
-      home: InfoSuperheroPage(),
     );
   }
 }
-
--- lib/services/superheroe_services.dart
+++ lib/services/superheroe_services.dart
@@ -1,37 +1,15 @@
-
-import 'dart:async';
-import 'dart:math';
+import 'package:flutter/material.dart';
 import 'package:flutter_codigo3_state/model/super_hero_model.dart';
 
-class _SuperheroeService{
+class SuperheroService with ChangeNotifier{
+
   Superheroe? _superheroe;
 
   Superheroe get superheroe => this._superheroe!;
 
-  StreamController<Superheroe> _superheroeStreamController = new StreamController<Superheroe>.broadcast();
-  Stream<Superheroe> get superheroeStream => _superheroeStreamController.stream;
-
-  loadSuperheroe(Superheroe superheroe){
+  set superheroe(Superheroe superheroe){
     _superheroe = superheroe;
-    _superheroeStreamController.add(_superheroe!);
+    notifyListeners();
   }
 
-  changeExperience(int experience){
-    _superheroe!.experience = experience;
-    _superheroeStreamController.add(_superheroe!);
-
-  }
-
-  addPowers(){
-    _superheroe!.powers.add("Power ${_superheroe!.powers.length + 1}");
-    _superheroeStreamController.add(_superheroe!);
-  }
-
-  bool get superheroeExist => (_superheroe != null) ? true : false;
-
-  closeStream(){
-    _superheroeStreamController.close();
-  }
 }
-
-final superheroeService = new _SuperheroeService();
